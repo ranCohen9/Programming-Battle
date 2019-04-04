838393447fd877bd335a498870656703a7d8d2f9
Solomon Pini <Pini.Solomon@elbitsystems.com>
2019-04-04 21:02:05 +0300

    avg on the many added http client test to GO

---
+++ HTTP/GO/Server/Server.go
@@ -0,0 +1,33 @@
+package main
+import(
+	"fmt"
+	"encoding/json"
+    //"log"
+    "net/http"
+	"github.com/gorilla/mux"
+)
+
+func main(){
+	fmt.Println("server starting");
+	router:=mux.NewRouter();
+	router.HandleFunc("/api/test/{id}", doGetMethod).Methods("GET");
+	router.HandleFunc("/api/test", doPostMethod).Methods("POST");
+	fmt.Println("server listening on port 8000");	
+	http.ListenAndServe(":8000",router);
+}
+
+func doGetMethod(w http.ResponseWriter, r *http.Request){
+	//fmt.Println("go get operation");	
+	params:=mux.Vars(r);
+	id:=params["id"];
+	json.NewEncoder(w).Encode(id);
+}
+
+func doPostMethod(w http.ResponseWriter, r *http.Request){
+	//fmt.Println("go post operation");
+	//fmt.Println(string(r.Body));
+	decoder:=json.NewDecoder(r.Body);
+	var obj json.RawMessage;
+	decoder.Decode(&obj);
+	json.NewEncoder(w).Encode(obj);
+}
